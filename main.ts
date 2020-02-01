9aad904ca06b12d05cb72721b685bd551a8d5aa6
jakubkoci <jakub.koci@gmail.com>
2020-02-01 13:49:43 +0100

    Reorganize code

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { Module, Injectable } from "@nestjs/common";
 
 @Injectable()
+class AppRepository {
+  sayHi() {
+    console.log("app repository");
+    console.log("Hello");
+  }
+}
+
+@Injectable()
 class AppService {
   constructor(private appRepository: AppRepository) {}
   sayHi() {
@@ -10,25 +18,22 @@ class AppService {
   }
 }
 
-@Injectable()
-class AppRepository {
-  sayHi() {
-    console.log("app repository");
-    console.log("Hello");
-  }
-}
-
 @Module({
   imports: [],
   providers: [AppService, AppRepository]
 })
-class AppModule {}
+class AppModule {
+  constructor(private appService: AppService) {}
+  sayHi() {
+    console.log("app module");
+    this.appService.sayHi();
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
-  console.log(app)
-  const service = app.get<AppService>(AppService);
-  service.sayHi();
+  const module = app.get<AppModule>(AppModule);
+  module.sayHi();
 }
 
 bootstrap();
